c54337b3c1235a7416b641fff2438a1dfd91a711
Daniel Silva <danielsilvavalencia25@gmail.com>
2022-09-22 00:56:18 -0500

    Se agrego Informe.class

---
+++ src/org/silva/poointerfaces/imprenta/modelo/Informe.java
@@ -0,0 +1,19 @@
+package org.silva.poointerfaces.imprenta.modelo;
+
+public class Informe extends Hoja{
+
+    private String autor;
+    private String revisor;
+
+    public Informe(String autor, String revisor, String contenido) {
+        super(contenido);
+        this.autor = autor;
+        this.revisor = revisor;
+    }
+    @Override
+    public String imprimir() {
+        return "Revisado por: " + autor
+                + "\nRevisado por: " + revisor
+                + "\nContenido: " + this.contenido;
+    }
+}
