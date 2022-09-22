0c9bf0bc822c7c372632e10d6d5d3ff89119ea91
Daniel Silva <danielsilvavalencia25@gmail.com>
2022-09-22 18:06:13 -0500

    EjemploImprenta 1era parte

--- src/org/silva/poointerfaces/imprenta/EjemploImprenta.java
+++ src/org/silva/poointerfaces/imprenta/EjemploImprenta.java
@@ -1,7 +1,20 @@
 package org.silva.poointerfaces.imprenta;
 
+import org.silva.poointerfaces.imprenta.modelo.Curriculo;
+import org.silva.poointerfaces.imprenta.modelo.Hoja;
+
 public class EjemploImprenta {
     public static void main(String[] args) {
+        Curriculo cv = new Curriculo("Daniel Silva Valencia",
+                "Ingenieria de sistemas", "Resumen laboral ...");
+        cv.addExperiencia("Java");
+        cv.addExperiencia("Git");
+        cv.addExperiencia("GitHub");
+        cv.addExperiencia("Vcode");
+        cv.addExperiencia("Html");
+    }
 
+    public static void imprimir(Hoja imprimible){
+        System.out.println(imprimible.imprimir());
     }
 }
