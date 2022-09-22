06999a0f82862649064cee4f918b9f8a5a981faa
Daniel Silva <danielsilvavalencia25@gmail.com>
2022-09-22 00:46:30 -0500

    Se agrego clase Hoja

--- src/org/silva/poointerfaces/imprenta/modelo/Hoja.java
+++ src/org/silva/poointerfaces/imprenta/modelo/Hoja.java
@@ -1,4 +1,11 @@
 package org.silva.poointerfaces.imprenta.modelo;
 
 abstract public class Hoja {
+    protected String contenido;
+
+    public Hoja(String contenido) {
+        this.contenido = contenido;
+    }
+
+    abstract public String imprimir();
 }
